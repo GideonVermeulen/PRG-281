2486ae016b84435ca0dfd6e8e2ea4f75bda7d323
FrederikWKnoetze <frederikwknoetze@gmail.com>
2024-08-20 21:17:36 +0200

    book

--- Library/Book.cs
+++ Library/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,27 @@ namespace Library
 {
     internal class Book
     {
+        private string ISBN;
+        private string Title;
+        private string Author;
+        private bool Borrowed;
+        private List<string> Genres;
+
+        public string isbn { get { return ISBN; } set { ISBN = value; } }
+        public string title { get { return Title; } set { Title = value; } }
+        public string author { get { return Author; } set { Author = value; } }
+        public bool borrowed { get { return Borrowed; } set { Borrowed = value; } }
+        public List<string> genres { get { return Genres; }set { Genres = value; } }
+
+        Book(string _ISBN, string _Author, string _Title, bool _Borrowed, List<string> _Genres) 
+        {
+            this.ISBN = _ISBN;
+            this.Title = _Title;
+            this.Author = _Author;
+            this.Borrowed = _Borrowed;
+            this.Genres = _Genres;
+        }
+
+        
     }
 }
--- Library/obj/Debug/net8.0-windows/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0-windows/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84370101aebfa33fa490be2a78fb397bfebc79c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed7c37a3beb64b89ee9e6e17ff7c7cdb58ed8a4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
