92ed6403491f64166dd4c445eb2477622e8ed829
FrederikWKnoetze <frederikwknoetze@gmail.com>
2024-08-20 21:39:50 +0200

    im suffering

--- Library/Form1.cs
+++ Library/Form1.cs
@@ -1,5 +1,6 @@
 namespace Library
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
@@ -9,7 +10,7 @@ namespace Library
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
---
+++ Library/GlobalData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class GlobalData
+    {
+         
+       static  List<Book> listBooks = new List<Book>();
+       
+
+    }
+}
